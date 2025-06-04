96d2e9d3c794738bc880b23222806d49826dc76f
Zahin Ahmed <155523057+zahin1080@users.noreply.github.com>
2025-06-04 17:34:34 +0600

    Update settings.py

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6y@a)7@b8vhzi-$1s-vunotdp#qdu%&ykkw^f0@$*%!)3hy#=3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['website-theory.onrender.com','localhost','127.0.0.1']
 
 
 # Application definition
