233a31d3ac734470fe56ecd43de87952756178a0
Zahin Ahmed <155523057+zahin1080@users.noreply.github.com>
2025-08-09 13:47:24 +0600

    Update settings.py

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-import dj_database_url
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
